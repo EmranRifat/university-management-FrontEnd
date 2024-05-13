7895173127a245cef834d804cf3a520b97cfc1f7
EmranRifat <imran15-1919@diu.edu.bd>
2024-05-13 08:26:53 -0700

    aaa

---
+++ src/app/(withlayout)/superAdmin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const superAdmin = () => {
+    return (
+        <div>
+           <h1>Super Admin page</h1> 
+        </div>
+    );
+};
+
+export default superAdmin
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/Providers";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +22,11 @@ export default function RootLayout({
   
   return (
     <Providers>
-      <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+       <AntdRegistry>
+       <html lang="en">
+       <body className={inter.className}>{children}</body>
+       </html>
+       </AntdRegistry>
     </Providers>
   );
 }
